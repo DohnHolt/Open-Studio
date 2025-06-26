abd502a4712883ce27ec3bef4192cccd743abddc
Tim <tim.d.rutherford@gmail.com>
2025-06-26 17:18:23 +0100

    Quartz sync: Jun 26, 2025, 5:18 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -65,7 +65,12 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [
+  Component.Breadcrumbs(),
+  Component.ArticleTitle(),
+  Component.ContentMeta(),
+  Component.ContentIndex({ showLastUpdated: false })  // 👈 hide the date here
+],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
